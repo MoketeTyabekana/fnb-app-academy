448e837af62f98fdd3233ed8a99e80b3a16f884a
Mokete <ematyabekana@gmail.com>
2025-06-05 09:08:42 +0200

    Initialize HTML and CSS files for calculator project and created 3 files for loading, home and js file.

---
+++ calulator/script.js
@@ -0,0 +1,7 @@
+    
+    // This script redirects the user to the calculator page after 3 seconds
+    window.onload = function() {
+         setTimeout(function() {
+             window.location.href = "calculator.html";
+         }, 3000);
+     };
