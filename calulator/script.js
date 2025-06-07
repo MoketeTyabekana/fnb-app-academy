b79e3d27fd7486baf2741a77e85e72bc0209b976
Mokete <ematyabekana@gmail.com>
2025-06-07 16:54:43 +0200

    Added calculator functionality and enhance loading screen design

--- calulator/script.js
+++ calulator/script.js
@@ -1,7 +1,96 @@
+
+let screen=document.getElementById('calculator-screen');
+let screenValue=screen.textContent;
+
+let firstValue=0;
+let previousOperator=null;
+let waitForSecondValue=false;
+
+
+function inputDigit(num){
+
+    if(waitForSecondValue){
+        waitForSecondValue=false;
+        screenValue=num;
+    }
+    else{
+        screenValue = screenValue ==='0'? num : screenValue + num;
+    }
     
-    // This script redirects the user to the calculator page after 3 seconds
-    window.onload = function() {
-         setTimeout(function() {
-             window.location.href = "calculator.html";
-         }, 3000);
-     };
+    updateScreenDisplay();
+}
+
+function inputDecimal(){
+    
+    if(waitForSecondValue){
+        inputDecimal('0.')
+    }
+    if(screenValue.includes('.')){
+        return;
+    }
+
+    if(!screenValue.includes('.')){
+        screenValue=screenValue+'.'
+    }
+    updateScreenDisplay();
+}
+
+function toggleSign(){
+    screenValue=screenValue*-1;
+    if(waitForSecondValue){
+        firstValue=screenValue;
+    }
+    updateScreenDisplay();
+}
+
+function clearEntry(){
+    screenValue=screenValue.slice(0,-1);
+    if(screenValue.length===0){
+        screenValue='0';
+    }
+    updateScreenDisplay();
+}
+
+function allClear(){
+    firstValue=0;
+    screenValue='0';
+    updateScreenDisplay();
+}
+
+function handleOperator(currentOperator){
+    
+    if(waitForSecondValue){
+        previousOperator=currentOperator;
+        return;
+    }
+
+
+    firstValue=calculate(firstValue,previousOperator,parseFloat(screenValue));
+    screenValue=firstValue;
+    previousOperator=currentOperator;
+    waitForSecondValue=true;
+    updateScreenDisplay();
+
+}
+
+function calculate(first,operator,second){
+    if(operator==='+')return first + second;
+    if(operator==='-')return first - second;
+    if(operator==='*')return first * second;
+    if(operator==='/')return first / second;
+    
+    return second;
+}
+
+function getSquareRoot(){
+    screenValue=Math.sqrt(parseFloat(screenValue));
+    firstValue=screenValue;
+    updateScreenDisplay();
+}
+
+
+function updateScreenDisplay(){
+    
+    screen.textContent=screenValue;  
+
+}
