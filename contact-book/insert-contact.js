bdf42d064aac02c3b86ade9260bdf4809c5f7ca8
Mokete <ematyabekana@gmail.com>
2025-06-15 19:07:19 +0200

    Refactored the submitForm function to include fetch logic for inserting contacts and enhance styling for contact buttons.

--- contact-book/insert-contact.js
+++ contact-book/insert-contact.js
@@ -6,4 +6,12 @@ function submitForm(e){
    const form = new FormData(document.querySelector('#editForm'));
 
    form.append('apiKey',apiKey);
+
+   fetch(rootPath + "controller/insert-contact", {
+       method: 'POST',
+       headers: {
+           'Accept': 'application/json',
+       },
+       body: form
+   })
 }
