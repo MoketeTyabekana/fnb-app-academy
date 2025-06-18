eccbc280b9a8c0b510e8126cfac9cdbd6c6aede3
Mokete <ematyabekana@gmail.com>
2025-06-18 10:30:26 +0200

    Add string manipulation examples in strings.py
    -strip
    -lower
    -upper
    -replace
    -split

--- python-basics/strings.py
+++ python-basics/strings.py
@@ -13,3 +13,19 @@ print(name[3])
 
 # string length
 print(len(name))
+
+# using strip to remove leading and trailing spaces
+
+print(name.strip())
+
+# using lower to convert to lowercase
+print(name.lower())
+
+# using upper to convert to uppercase
+print(name.upper())
+
+# using replace to replace a substring
+print(name.replace("Alice", "Bob"))
+
+# using split to split a string into a list
+print(message.split())
