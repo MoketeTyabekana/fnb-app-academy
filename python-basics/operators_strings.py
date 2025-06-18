66df5bf952dd162b668c2655b16fd057c7dce74f
Mokete <ematyabekana@gmail.com>
2025-06-18 11:10:52 +0200

    Added operators_strings.py with examples of string concatenation and repetition

---
+++ python-basics/operators_strings.py
@@ -0,0 +1,14 @@
+# Operators on Strings
+
+name = "John"
+# Concatenation
+greeting = "Hello, " + name + "!"  
+print(greeting)  
+# Output: Hello, John!
+
+# Repetition
+repeat_greeting = greeting * 3
+print(repeat_greeting)  
+# Output: Hello, John!Hello, John!Hello, John!
+
+
