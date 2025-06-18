cb14b40acdbb28ccf82b393bff4b2399eeba7abd
Mokete <ematyabekana@gmail.com>
2025-06-18 10:24:15 +0200

    Created a new string variable with value and accessed the specific charecters using square brackets annotations, and also used the length function to get the length of the string.

--- python-basics/strings.py
+++ python-basics/strings.py
@@ -2,5 +2,14 @@
 
 mesaage = """Hello, World!
 is cool"""
-
+# 
 print(mesaage)
+
+# advanced string formatting
+name = " Alice"
+# Accessing the first character
+print(name[0])  
+print(name[3])
+
+# string length
+print(len(name))
