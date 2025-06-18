f35da44eddb3f5a505bcadb1737ad1720fb4b365
Mokete <ematyabekana@gmail.com>
2025-06-18 10:51:51 +0200

    Added numeric_data.py with examples of integer, float, and complex data types

---
+++ python-basics/numeric_data.py
@@ -0,0 +1,15 @@
+# Numeric Data
+# integer, float, complex
+num=10
+print(num)
+print(type(num))
+
+# float
+num=10.5
+print(num)
+print(type(num))
+
+# complex
+num=10+5j
+print(num)
+print(type(num))
