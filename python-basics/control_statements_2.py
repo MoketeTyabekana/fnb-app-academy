0d88b8dfce69869d59633fce6d3b50a82076ca72
Mokete <ematyabekana@gmail.com>
2025-06-18 11:23:40 +0200

    Added user input handling and if-else statements to control_statements_2.py

--- python-basics/control_statements_2.py
+++ python-basics/control_statements_2.py
@@ -1,6 +1,16 @@
 # Control Statements Part 2
+# user input
 
-
+number = int(input("Enter a number: "))
+number2 = int(input("Enter another number: "))
+# if-else statement with user input
+if number > number2:
+    print(f"{number} is greater than {number2}")
+elif number == number2:
+    print(f"{number} is equal to {number2}")
+else:
+    print(f"{number} is not greater than {number2}")
+    
 
 
 
