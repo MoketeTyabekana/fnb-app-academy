6697ef418bd17f0aad8d5d91492196d40746d09d
Mokete <ematyabekana@gmail.com>
2025-06-18 11:05:03 +0200

    Added operators_numeric_data.py with examples of numeric operations

---
+++ python-basics/operators_numeric_data.py
@@ -0,0 +1,20 @@
+# Operators on Numeric Data
+
+# addition(+)
+# subtraction(-)
+# multiplication(*)
+# division(/)
+# floor division(//)
+# modulus(%)
+# exponentiation(**)
+
+x=10
+y=5
+
+print(x+y)  # addition
+print(x-y)  # subtraction
+print(x*y)  # multiplication
+print(x/y)  # division  
+print(x//y) # floor division
+print(x%y)  # modulus
+print(x**y) # exponentiation
