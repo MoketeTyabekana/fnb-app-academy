927b60d137f6a532d5c36e7459f30adf45a43efc
Mokete <ematyabekana@gmail.com>
2025-06-18 11:30:32 +0200

    Add loops.py with examples of for and while loops

---
+++ python-basics/loops.py
@@ -0,0 +1,21 @@
+# Loops
+# for loop-is used to iterate over a sequence (like a list, tuple, or string) or other iterable objects.
+# while loop-is used to execute a block of code as long as a condition is true.
+
+cars = ["Toyota", "Honda", "Ford", "Chevrolet"]
+
+for car in cars:
+    print(car)
+    
+print("----------------------------------------------")
+# Using a for loop with range
+for i in range(5):
+    print(i)
+    
+    
+print("----------------------------------------------")
+# Using a while loop
+count = 0
+while count < 5:
+    print(count)
+    count += 1
