677cd5b21abece65bbf8c956e0637b3ad4802e2b
Mokete <ematyabekana@gmail.com>
2025-06-24 21:02:46 +0200

    Added tuple concatenation example to tuples.py

--- python-basics/tuples.py
+++ python-basics/tuples.py
@@ -8,3 +8,11 @@ print(tuplets[2])
 
 # Using negation index to get a specific item
 print(tuplets[-1])
+
+# Combing two tuplets together.
+
+t1=(1,2,3)
+t2=(4,5,6)
+
+newtuplets=t1+t2
+print(newtuplets)
