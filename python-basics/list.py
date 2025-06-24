b0274dceab43fd884f9e5e7fa62eb18b776b3846
Mokete <ematyabekana@gmail.com>
2025-06-24 20:37:41 +0200

    Added list.py with basic list operations and examples

---
+++ python-basics/list.py
@@ -0,0 +1,22 @@
+# Lists
+
+fruits=["Banana","Apple","Cherry"]
+
+print(fruits[0])
+
+fruits[1]="Blueberry"
+print(fruits[1])
+
+# Removing Items from the list.
+fruits.remove(fruits[0])
+print(fruits)
+
+# Adding a new item into the list.
+fruits.append("Kiwi")
+print(fruits)
+
+
+# Adding new item into a specific position.
+
+fruits.insert(0,"Orange")
+print(fruits)
