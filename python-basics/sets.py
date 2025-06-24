2293ca68a5f4c7ef05ed5a4c6ece9e0a1aae0b83
Mokete <ematyabekana@gmail.com>
2025-06-24 21:41:05 +0200

    Added set operations including union, intersection, and difference methods to sets.py

--- python-basics/sets.py
+++ python-basics/sets.py
@@ -11,3 +11,20 @@ print(my_sets)
 # Removing an item from our set
 my_sets.remove(3)
 print(my_sets)
+
+# New sets
+
+set1={1,2,3}
+set2={3,4,5,6}
+
+# Union method
+newSet=set1.union(set2)
+print(newSet)
+
+# Interception method
+inter_set=set2.intersection(set1)
+print(inter_set)
+
+# Difference Method
+dif_set=set1.difference(set2)
+print(dif_set)
