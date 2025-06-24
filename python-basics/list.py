40450635f62ebe71f378baf9009cebe3f8bb2186
Mokete <ematyabekana@gmail.com>
2025-06-24 20:50:55 +0200

    Explore new python methods like sort,insert, append and remove.

--- python-basics/list.py
+++ python-basics/list.py
@@ -1,9 +1,10 @@
 # Lists
 
+# Declaring and printing the list
 fruits=["Banana","Apple","Cherry"]
-
 print(fruits[0])
 
+# Inserting a new item at a specific
 fruits[1]="Blueberry"
 print(fruits[1])
 
@@ -17,6 +18,14 @@ print(fruits)
 
 
 # Adding new item into a specific position.
-
 fruits.insert(0,"Orange")
 print(fruits)
+
+# Sorting out the list items
+fruits.sort()
+print(fruits)
+
+fruits.sort(reverse=True)
+print(fruits)
+
+
