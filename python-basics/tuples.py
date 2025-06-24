246756e446a26d2b3db25e6004ce825c7d214548
Mokete <ematyabekana@gmail.com>
2025-06-24 21:00:33 +0200

    printing a specific item from the tuplets using square annotation, and also negation index.

--- python-basics/tuples.py
+++ python-basics/tuples.py
@@ -1,3 +1,10 @@
 # Tuplets: They allow duplication, order function and cannot be changed.
 
-tuplets=(1,2,3)
+tuplets=(1,2,3,4,5)
+print(tuplets)
+
+# Print a single item
+print(tuplets[2])
+
+# Using negation index to get a specific item
+print(tuplets[-1])
