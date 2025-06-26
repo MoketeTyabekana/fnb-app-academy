152515cbcbb90ff4eb91166cb850c29e2bad9eb7
Mokete <ematyabekana@gmail.com>
2025-06-26 17:16:46 +0200

    Fix case sensitivity in dictionary keys and ensure consistent print statements

--- python-basics/dictionaries.py
+++ python-basics/dictionaries.py
@@ -1,5 +1,14 @@
 # Dictionaries
 
-my_dic={"apple":3,"Banana":5,"Orange":7}
+my_dic={"Apple":3,"Banana":5,"Orange":7}
 
-print(my_dic["apple"])
+print(my_dic["Apple"])
+
+# New Item
+
+my_dic["Grape"]=4
+print(my_dic)
+
+# Updating key value
+my_dic["Banana"]=10
+print(my_dic)
