df21bdd38a8ade52887924fda980ca5bd0bfa58a
Mokete <ematyabekana@gmail.com>
2025-07-01 16:09:14 +0200

    Refactored the output formatting in functions module for improved readability

--- functions-and-modules/functions.py
+++ functions-and-modules/functions.py
@@ -7,7 +7,8 @@ def greet(name):
 greet("Mokete")
 
 
-
+print("\n")
+print("---------------------")
 # Using the return statement
 
 def add_numbers(a, b):
@@ -15,4 +16,27 @@ def add_numbers(a, b):
 
 # calling the function and storing the result
 result = add_numbers(5, 3)
+print("\n")
 print(f"The sum is: {result}")
+
+print("\n")
+print("---------------------")
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+    
+print(f"The factorial of 5 is: {factorial(5)}")
+
+print("\n")
+print("---------------------")
+
+def greeting(name, greeting="Hello"):
+    return f"{greeting}, {name}!"
+# calling the function with default greeting
+print(greeting("Mokete"))
+
+# calling the function with custom greeting    
+print(greeting("Mokete", "Welcome"))
